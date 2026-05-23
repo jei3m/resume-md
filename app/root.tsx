61fd95422f65f5d3b5d8fa01c4aaadf80a8ff6db
Justin Miguel Reyes <virozzegt@gmail.com>
2026-05-23 17:35:25 +0800

    feat: apply toast

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router"
 
 import type { Route } from "./+types/root"
+import { Toaster } from "~/components/ui/sonner"
 import "./app.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -21,6 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
